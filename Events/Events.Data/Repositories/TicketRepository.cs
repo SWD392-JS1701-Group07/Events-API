013dfea50c5aa51f0d236a1180b7738385dcb4d7
NET_05_VanHoangTIen <tienvhse172309@fpt.edu.vn>
2024-06-21 15:53:07 +0700

    add email helper and modify create account

---
+++ Events/Events.Data/Repositories/TicketRepository.cs
@@ -0,0 +1,26 @@
+﻿using Events.Data.Repositories.Interfaces;
+using Events.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Data.Repositories
+{
+    public class TicketRepository : ITicketRepository
+    {
+        private readonly EventsDbContext _context;
+
+        public TicketRepository(EventsDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CreateTicket(Ticket ticket)
+        {
+            await _context.Tickets.AddAsync(ticket);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
