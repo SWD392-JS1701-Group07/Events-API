ad5061e77dee0cfb0077447a550a6bc8c67b139a
HoangTien0105 <hoangtien01052003@gmail.com>
2024-06-30 02:44:05 +0700

    add ISubjectRepository

---
+++ Events/Events.Data/Repositories/Interfaces/ISubjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Events.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Data.Repositories.Interfaces
+{
+    public interface ISubjectRepository
+    {
+        Task<Subject> GetSubjectById(int id);
+    }
+}
