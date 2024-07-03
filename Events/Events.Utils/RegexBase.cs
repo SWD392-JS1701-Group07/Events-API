70c345b24225ee84fa60b1a6f43c901322949bb7
kenzongo <overdev.khang@gmail.com>
2024-07-03 22:36:22 +0700

    add RegexBase for Validate

---
+++ Events/Events.Utils/RegexBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Utils
+{
+	public class RegexBase
+	{
+		public const string PhoneNumberRegex = @"^(0|\+84|84)(3|5|7|8|9)[0-9]{8}$";
+		public const string MustNumberRegex = @"^\d+(\.\d+)?$";
+		public const string SingleSpaceCharacterRegex = @"^[^\s\d]+(\s[^\s\d]+)*$";
+		public const string ErrorMessageSingleSpaceCharacterRegex = "only charactor and does not contain multiple consecutive spaces";
+	}
+}
