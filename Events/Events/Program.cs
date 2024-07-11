4968654366500baa8856120a3ec37ac25f155f77
kenzongo <overdev.khang@gmail.com>
2024-07-12 03:34:36 +0700

    edit program.cs

--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -26,7 +26,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<EventsDbContext>(
-    option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+	option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -34,82 +34,82 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddApiVersioning(options =>
 {
-    options.ReportApiVersions = true; 
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.Conventions.Controller<EventController>().HasApiVersion(new Microsoft.AspNetCore.Mvc.ApiVersion(2, 0));
+	options.ReportApiVersions = true;
+	options.DefaultApiVersion = new ApiVersion(1, 0);
+	options.Conventions.Controller<EventController>().HasApiVersion(new Microsoft.AspNetCore.Mvc.ApiVersion(2, 0));
 });
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "V1.0",
-        Title = "API V1",
-    });
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "V1.0",
+		Title = "API V1",
+	});
 
-    c.SwaggerDoc("v2", new OpenApiInfo
-    {
-        Version = "V2.0",
-        Title = "API V2",
-    });
+	c.SwaggerDoc("v2", new OpenApiInfo
+	{
+		Version = "V2.0",
+		Title = "API V2",
+	});
 
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer"
-    });
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = "JWT Authorization header using the Bearer scheme",
+		Type = SecuritySchemeType.Http,
+		Scheme = "bearer"
+	});
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "Bearer"}
-            },
-            Array.Empty<string>()
-        }
-    });
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "Bearer"}
+			},
+			Array.Empty<string>()
+		}
+	});
 
-    // Set the comments path for the Swagger JSON and UI.
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
+	// Set the comments path for the Swagger JSON and UI.
+	var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+	c.IncludeXmlComments(xmlPath);
 });
 
 //CLoudinary
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("Cloudinary"));
 //Authentication
-builder.Services.AddAuthentication(options => 
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme).
-    AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-      //      ValidIssuer = builder.Configuration["JWT:Issuer"],
-            ValidateAudience = false,
-      //      ValidAudience = builder.Configuration["JWT:Audience"],
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            RoleClaimType = ClaimTypes.Role,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1c4890495b93b9e71fee12bf1880242771ad287f814d9553b120de5b82428b0b"))
-        };
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                var token = context.SecurityToken as JwtSecurityToken;
-                if (token != null && !JWTGenerator.IsTokenValid(token.RawData))
-                {
-                    context.Fail("Token is invalid");
-                }
-                return Task.CompletedTask;
-            }
-        };
-    });
-    
+builder.Services.AddAuthentication(options =>
+	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme).
+	AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuer = false,
+			//      ValidIssuer = builder.Configuration["JWT:Issuer"],
+			ValidateAudience = false,
+			//      ValidAudience = builder.Configuration["JWT:Audience"],
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+			RoleClaimType = ClaimTypes.Role,
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1c4890495b93b9e71fee12bf1880242771ad287f814d9553b120de5b82428b0b"))
+		};
+		options.Events = new JwtBearerEvents
+		{
+			OnTokenValidated = context =>
+			{
+				var token = context.SecurityToken as JwtSecurityToken;
+				if (token != null && !JWTGenerator.IsTokenValid(token.RawData))
+				{
+					context.Fail("Token is invalid");
+				}
+				return Task.CompletedTask;
+			}
+		};
+	});
+
 builder.Services.AddAutoMapper(typeof(Events.Business.Mapping.MappingProfiles));
 
 builder.Services.AddScoped<IEventRepository, EventRepository>();
@@ -147,7 +147,18 @@ builder.Services.AddScoped<QrHelper>();
 
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policyBuilder =>
-        policyBuilder.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader().AllowCredentials()));
+{
+	var frontendUrl = builder.Configuration.GetValue<string>("AppFrontend");
+	if (string.IsNullOrEmpty(frontendUrl))
+	{
+		throw new ArgumentNullException("AppFrontend", "Frontend URL cannot be null or empty.");
+	}
+	policyBuilder.WithOrigins(frontendUrl)
+					 .AllowAnyMethod()
+					 .AllowAnyHeader()
+					 .AllowCredentials();
+}));
+		
 
 // Register the background service
 //builder.Services.AddHostedService<EventStatusUpdateService>();
@@ -155,35 +166,33 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policyBuilder =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
-        c.SwaggerEndpoint("/swagger/v2/swagger.json", "API V2");
-        c.RoutePrefix = "swagger";
-    });
+	app.UseSwagger();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+		c.SwaggerEndpoint("/swagger/v2/swagger.json", "API V2");
+		c.RoutePrefix = "swagger";
+	});
+	app.UseHttpsRedirection();
+
+	string imagePath = Path.Combine(builder.Environment.ContentRootPath, "Images");
+	if (!Directory.Exists(imagePath))
+	{
+		Directory.CreateDirectory(imagePath);
+	}
+	app.UseStaticFiles(new StaticFileOptions
+	{
+		FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Images")),
+		RequestPath = "/Images"
+	});
+
+	app.UseAuthentication();
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.UseCors();
+
+	app.Run();
 }
-app.UseHttpsRedirection();
-
-string imagePath = Path.Combine(builder.Environment.ContentRootPath, "Images");
-if(!Directory.Exists(imagePath))
-{
-    Directory.CreateDirectory(imagePath);
-}
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Images")),
-    RequestPath = "/Images"
-});
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseCors();
-
-app.Run();
-
