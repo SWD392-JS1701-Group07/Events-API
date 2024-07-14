e3e4c930a99ea28dfea68af5cfa3ad3e75cce393
HoangTien0105 <hoangtien01052003@gmail.com>
2024-07-14 23:59:03 +0700

    fix sponsorships bug

--- Events/Events/Controllers/SponsorshipController.cs
+++ Events/Events/Controllers/SponsorshipController.cs
@@ -38,7 +38,7 @@ namespace Events.API.Controllers
 
         [HttpGet("sponsors/{id}")]
         [Authorize(Roles = "1,3,4,5")]
-        public async Task<IActionResult> GetSponsorshipBySponsorId([FromQuery] int id, string? searchTerm, string? sortColumn, string? sortOrder, int page, int pageSize)
+        public async Task<IActionResult> GetSponsorshipBySponsorId(int id, [FromQuery] string? searchTerm, string? sortColumn, string? sortOrder, int page, int pageSize)
         {
             var result = await _sponsorshipService.GetSponsorshipBySponsorId(id, searchTerm, sortColumn, sortOrder, page, pageSize);
             return StatusCode(result.StatusCode, result);
